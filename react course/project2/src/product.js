054a13ec327edf3f42c649f289f057a860581238
Loubna Kamal <loubnakamaly1998@gmail.com>
2023-07-19 16:11:41 +0300

    editing

--- react course/project2/src/product.js
+++
@@ -1,7 +0,0 @@
-export const product =[ {
-    name: "React Course",
-    price: 5000,
-    description: "Learning development of websites using react.",
-    image_url: "https://example.com/product-image.jpg"
-  }
-];
